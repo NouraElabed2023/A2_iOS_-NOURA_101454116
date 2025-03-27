af5985d72035ce8a2f6f4a8d204d7d8ae381cf37
NouraElabed2023 <rahim.abbas27@gmail.com>
2025-03-28 01:53:08 +0500

    Initial Commit

---
+++ Noura Elabed/Noura_ElabedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Noura_ElabedApp.swift
+//  Noura Elabed
+//
+//  Created by Rahim Abbas on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Noura_ElabedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Noura ElabedTests/Noura_ElabedTests.swift
@@ -0,0 +1,17 @@
+//
+//  Noura_ElabedTests.swift
+//  Noura ElabedTests
+//
+//  Created by Rahim Abbas on 3/28/25.
+//
+
+import Testing
+@testable import Noura_Elabed
+
+struct Noura_ElabedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
