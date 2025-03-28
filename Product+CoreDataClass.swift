1a68f413bf86c1516c735ce83336283ebfc036ef
NouraElabed2023 <rahim.abbas27@gmail.com>
2025-03-28 09:53:30 +0500

    Made some minor changes to the "Noura_ElabedApp" File

---
+++ Product+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Product+CoreDataClass.swift
+//  Noura Elabed
+//
+//  Created by Rahim Abbas on 3/28/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Product)
+public class Product: NSManagedObject {
+
+}
