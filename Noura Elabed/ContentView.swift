7f20281d933ee533ee48315d06d3a91783855108
NouraElabed2023 <rahim.abbas27@gmail.com>
2025-03-28 09:57:03 +0500

    Set up PersistenceController Class

--- Noura Elabed/ContentView.swift
+++ Noura Elabed/ContentView.swift
@@ -1,8 +1,8 @@
-
-
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -13,6 +13,22 @@ struct ContentView: View {
         .padding()
     }
 }
+
+
+class PersistenceController {
+    static let shared = PersistenceController()
+    let container: NSPersistentContainer
+    
+    init() {
+        container = NSPersistentContainer(name: "Product")
+        container.loadPersistentStores { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
--- Product+CoreDataProperties.swift
+++ Product+CoreDataProperties.swift
@@ -1,11 +1,3 @@
-//
-//  Product+CoreDataProperties.swift
-//  Noura Elabed
-//
-//  Created by Rahim Abbas on 3/28/25.
-//
-//
-
 import Foundation
 import CoreData
 
