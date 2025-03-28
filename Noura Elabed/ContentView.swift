c3fdb8516b268b976188516bb2d9f2a048b4aed2
NouraElabed2023 <rahim.abbas27@gmail.com>
2025-03-28 10:02:54 +0500

    Set up a ProductDetailView Struct to display the products

--- Noura Elabed/ContentView.swift
+++ Noura Elabed/ContentView.swift
@@ -15,6 +15,25 @@ struct ContentView: View {
 }
 
 
+struct ProductDetailView: View {
+    var product: Product
+    
+    var body: some View {
+        VStack {
+            Text(product.name ?? "Unknown")
+                .font(.largeTitle)
+                .padding()
+            Text(product.productDescription ?? "No description")
+                .font(.body)
+                .padding()
+            Text("Price: $\(product.price, specifier: "%.2f")")
+                .font(.headline)
+            Text("Provider: \(product.provider ?? "Unknown")")
+                .font(.subheadline)
+        }
+    }
+}
+
 class PersistenceController {
     static let shared = PersistenceController()
     let container: NSPersistentContainer
